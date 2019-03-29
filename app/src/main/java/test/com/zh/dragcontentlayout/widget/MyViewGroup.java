c51f95e27e654bc80253e58c93cec5dc118235f1
zhaoh <zhaoh@126.com>
2019-03-29 17:48:44 +0800

    https://github.com/zhao2017/DragContentLayout.git

--- app/src/main/java/test/com/zh/dragcontentlayout/widget/MyViewGroup.java
+++ app/src/main/java/test/com/zh/dragcontentlayout/widget/MyViewGroup.java
@@ -27,9 +27,47 @@ public class MyViewGroup extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int cCount = getChildCount();
+        int cWidth = 0;
+        int cHeight = 0;
+        MarginLayoutParams cParams = null;
+        /**
+         * 遍历所有childView根据其宽和高，以及margin进行布局
+         */
+        for (int i = 0; i < cCount; i++) {
+            View childView = getChildAt(i);
+            cWidth = childView.getMeasuredWidth();
+            cHeight = childView.getMeasuredHeight();
+            cParams = (MarginLayoutParams) childView.getLayoutParams();
+            int cl = 0, ct = 0, cr = 0, cb = 0;
+            switch (i) {
+                case 0:
+                    cl = cParams.leftMargin;
+                    ct = cParams.topMargin;
+                    break;
+                case 1:
+                    cl = getWidth() - cWidth - cParams.leftMargin
+                            - cParams.rightMargin;
+                    ct = cParams.topMargin;
 
+                    break;
+                case 2:
+                    cl = cParams.leftMargin;
+                    ct = getHeight() - cHeight - cParams.bottomMargin;
+                    break;
+                case 3:
+                    cl = getWidth() - cWidth - cParams.leftMargin
+                            - cParams.rightMargin;
+                    ct = getHeight() - cHeight - cParams.bottomMargin;
+                    break;
+
+            }
+            cr = cl + cWidth;
+            cb = cHeight + ct;
+            childView.layout(cl, ct, cr, cb);
+
+        }
     }
-
     /**
      *  计算所有的childView的宽度和高度，然后根据childView的计算结果设置自己的宽高。
      *
@@ -51,6 +89,12 @@ public class MyViewGroup extends ViewGroup {
 
         // 计算所有childView的宽和高
         measureChildren(widthMeasureSpec,heightMeasureSpec);
+
+         /*
+        记录如果是wrap_content是设置的宽和高
+                */
+        int width = 0;
+        int height = 0;
         int cCount = getChildCount();
         int cWidth = 0;
         int cHeight = 0;
@@ -70,16 +114,35 @@ public class MyViewGroup extends ViewGroup {
          */
         for (int i = 0; i <cCount ; i++) {
             View childView = getChildAt(i);
-
             cWidth = childView.getMeasuredWidth();
             cHeight = childView.getMeasuredHeight();
+            cParams = (MarginLayoutParams) childView.getLayoutParams();
+            if(i==0||i==1){
+                tWidth+=cWidth+cParams.leftMargin+cParams.rightMargin;
+            }
 
+            if(i==2||i==3){
+                bWidth += cWidth + cParams.leftMargin + cParams.rightMargin;
+            }
+            if (i == 0 || i == 2)
+            {
+                lHeight += cHeight + cParams.topMargin + cParams.bottomMargin;
+            }
 
-
+            if (i == 1 || i == 3)
+            {
+                rHeight += cHeight + cParams.topMargin + cParams.bottomMargin;
+            }
 
         }
-
+        width = Math.max(tWidth, bWidth);
+        height = Math.max(lHeight, rHeight);
+        setMeasuredDimension((withMode==MeasureSpec.EXACTLY?sizeWith:width),(heightMode==MeasureSpec.EXACTLY?sizeHeight:height));
     }
+
+
+
+
 
     /**
      * 重写父类的该方法，返回MarginLayoutParams的实例，这样就为我们的ViewGroup指定了其LayoutParams为MarginLayoutParams。
